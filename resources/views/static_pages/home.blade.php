92be3f2457fc9b633a4444bbaab96281bcc02a6f
yzx <yzx@zhenxing.site>
2017-11-30 12:05:49 +0800

    完成布局和路由

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -10,7 +10,7 @@
             <a target="_blank" href="https://d.laravel-china.org/">Laravel文档中心</a>
         </p>
         <p>
-           <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+           <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
         </p>
     </div>
 @stop
