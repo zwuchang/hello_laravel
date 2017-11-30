9ccb7eec210a6dda2227b798dcd96f3a524d8f9f
yzx <yzx@zhenxing.site>
2017-11-30 16:38:12 +0800

    更改数据库配置

--- config/database.php
+++ config/database.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
 
     /*
     |--------------------------------------------------------------------------
